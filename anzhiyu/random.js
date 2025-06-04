9873e9c2020a4511c57d7414845a7442c7950476
zejiew <1324639842@qq.com>
2025-06-05 00:22:41 +0800

    Site updated: 2025-06-05 00:22:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/25/hello-world/","2025/04/26/这是一篇测试文章/"];function toRandomPost(){
+var posts=["2025/04/26/这是一篇测试文章/","2025/04/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
