e84da62d6ded9d5ec249823e0cd9c78b72fb146e
zejiew <1324639842@qq.com>
2025-06-05 14:17:45 +0800

    Site updated: 2025-06-05 14:17:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/25/hello-world/","2025/06/05/集成AI的超好用的PDF编辑器阅读器-PDFgear-1/","2025/04/26/这是一篇测试文章/","2025/06/05/集成AI的超好用的PDF编辑器阅读器-PDFgear/"];function toRandomPost(){
+var posts=["2025/04/25/hello-world/","2025/04/26/这是一篇测试文章/","2025/06/05/集成AI的超好用的PDF编辑器阅读器-PDFgear-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
