7eb2be4c0067318a78cc8bf12a5a2aa6980cd2f4
zejiew <1324639842@qq.com>
2025-06-07 15:13:14 +0800

    Site updated: 2025-06-07 15:13:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/05/WindowsCleaner：专治C盘爆红的开源清理工具/","2025/04/25/hello-world/","2025/04/26/这是一篇测试文章/"];function toRandomPost(){
+var posts=["2025/04/25/hello-world/","2025/04/26/这是一篇测试文章/","2025/06/05/WindowsCleaner：专治C盘爆红的开源清理工具/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
